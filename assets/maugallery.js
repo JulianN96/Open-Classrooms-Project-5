7bd4f98f9fdc31813e707af5f9c272d19d284c93
Julian Norris <julian.norris96@gmail.com>
2023-09-25 13:03:08 +0200

    Updated some photo formats. Fixed the issues with the links overshooting the content. Fixed the image carousel in the modal

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -153,8 +153,9 @@
           index = i ;
         }
       });
+      console.log(index)
       next =
-        imagesCollection[index] ||
+        imagesCollection[index -1] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +193,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index +1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
